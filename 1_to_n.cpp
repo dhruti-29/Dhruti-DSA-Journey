236e36f617b82222fbc09cee2a40fca058ca9aec
dhruti viradiya <25cs113@charusat.edu.in>
2026-05-29 01:28:56 +0530

    add new cpp code file

---
+++ 1_to_n.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+void call(int n){
+    if(n==0){
+        return;
+    }
+    call(n-1);
+    cout<<n<<endl;
+}
+int main() {
+    call(5);
+    return 0;
+}
