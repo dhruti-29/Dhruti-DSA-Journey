e1afe553cfecf69342dff76eea12cc5f6ff9e5ea
dhruti viradiya <25cs113@charusat.edu.in>
2026-05-29 01:29:35 +0530

    add new file

---
+++ arrays.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int aray(int n,int a[]){
+    if(n==0){
+
+    }
+    cout<<"ans : "<<
+}
+int main() {
+    int b[5]={1,2,3,4,5};
+    aray(5 ,b);
+    return 0;
+}
