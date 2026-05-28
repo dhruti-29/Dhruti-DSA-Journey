c1e4fd38b117c3901ee9e87909ac32692cdbb19a
dhruti viradiya <25cs113@charusat.edu.in>
2026-05-29 01:30:50 +0530

    add new code file

---
+++ Fibonacciseries.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+//0 1 1 2 3 5 
+int fibo(int n){
+
+    if(n==0){return 0;}
+    if(n==1 || n==2){return 1;}
+
+    return fibo(n-1) + fibo(n-2);
+
+}
+
+int main() {
+   cout<<fibo(4);
+    return 0;
+}
