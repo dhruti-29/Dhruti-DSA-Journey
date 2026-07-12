516dd7b41019eba4f1c0fe4f213564d5226020f4
dhruti viradiya <25cs113@charusat.edu.in>
2026-07-12 23:32:21 +0530

    add code file

---
+++ patterns/1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    for(int i =0;i<4;i++){
+
+        int k = 1;
+        for(int j =0;j<4;j++){
+printf("%d ",k);
+k++;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
