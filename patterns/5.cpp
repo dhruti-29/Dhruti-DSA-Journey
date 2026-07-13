3d2dc738d843a224c2b107b2e53b33331ff3cd00
dhruti viradiya <25cs113@charusat.edu.in>
2026-07-13 23:08:54 +0530

    add code file\

---
+++ patterns/5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+char k = 'A';
+for(int i = 0;i<5;i++){
+
+        for(int j =0; j <5 ;j++){
+
+            cout<<k<<" ";
+            k++;
+        }
+        cout<<endl;
+
+    }
+    return 0;
+}
