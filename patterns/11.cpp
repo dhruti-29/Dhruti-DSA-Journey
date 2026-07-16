5db9160f36daaa94baca25cad857c98922ce833b
dhruti viradiya <25cs113@charusat.edu.in>
2026-07-16 22:59:18 +0530

    add file

---
+++ patterns/11.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    
+    int k = 1;
+    for(int i = 0;i<5;i++){
+
+        for(int j =0; j <=i ;j++){
+
+            cout<<k<<" ";
+            k++;
+        }
+        cout<<endl;
+
+    }
+    return 0;
+}
