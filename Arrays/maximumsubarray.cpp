944398be042733ba715241fed8b9eafda737b467
dhruti viradiya <25cs113@charusat.edu.in>
2026-07-25 14:17:12 +0530

    add code file

---
+++ Arrays/maximumsubarray.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int n=5;
+    int arr[5] ={1,2,3,4,5};
+
+    n^ 3 = complexity 
+   
+   for(int st =0;st<n;st++){
+        for(int end = st;end<n;end++){
+           
+            for(int i = st; i<=end; i++){
+                cout<<arr[i];
+            }
+            cout<<" ";
+        }
+        cout<<" "<<endl; n^ 2 = complexity
+
+    } 
+
+
+    
+
+    return 0;
+}
